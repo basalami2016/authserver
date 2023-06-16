e67e977768bec2d82e83a56c5c220e28d45218cc
BILIAMINU SALAMI <basalami2016@gmail.com>
2023-06-16 19:46:08 -0400

    commit update

--- src/main/java/io/niqflex/authserver/authn/AuthorizationServerConfig.java
+++ src/main/java/io/niqflex/authserver/authn/AuthorizationServerConfig.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+updated
+*/
 package io.niqflex.authserver.authn;
 
 import java.time.Duration;
